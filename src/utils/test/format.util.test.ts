67d1de10c03651a02de84e804ca08a417a66f0b5
Jose Castillo <jcastillovnz@gmail.com>
2023-10-01 20:56:21 -0300

    feat: test format forecasts list

--- src/utils/test/format.util.test.ts
+++ src/utils/test/format.util.test.ts
@@ -1,20 +1,12 @@
 import { formatUtil } from "../format.util"; // Asegúrate de ajustar la ruta correcta
 import { describe, expect, it, vi } from "vitest";
-import {
-  currentForecastExpected,
-  nexFiveDaysForecastExpected,
-  weatherResponse,
-} from "./mock";
+import { nexFiveDaysForecastExpected, weatherResponse } from "./mock";
 describe("formatUtil", () => {
-  it.only("should format response correctly", () => {
+  it.only("should format forecasts and return 5", () => {
     const mockDate = new Date("2023-10-01T12:55:00Z");
     vi.setSystemTime(mockDate);
-    const formattedWeather = formatUtil.formatForescastsResponse(weatherResponse);
-/*     expect(formattedWeather.id).toBe(3862744);
-    expect(formattedWeather.name).toBe("Departamento de Capital"); */
-    expect(formattedWeather).toStrictEqual(
-      currentForecastExpected
-    );
-    expect(formattedWeather).toStrictEqual(nexFiveDaysForecastExpected);
+    const forecastFormated =
+      formatUtil.formatForescastsResponse(weatherResponse);
+    expect(forecastFormated).toStrictEqual(nexFiveDaysForecastExpected);
   });
 });
