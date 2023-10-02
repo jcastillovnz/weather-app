86487f9a97af4995f0089783b5ea2eedb5f02f94
Jose Castillo <jcastillovnz@gmail.com>
2023-10-02 01:09:14 -0300

    refactor: uselocation hook

--- src/container/weather/hooks/useGeolocationWeather.ts
+++ src/container/weather/hooks/useGeolocationWeather.ts
@@ -1,31 +1,75 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { weatherSagasActions } from "../../../store/features";
+import { weatherSagasActions, weatherActions } from "../../../store/features";
 
 export function useGeolocationWeather() {
   const dispatch = useDispatch();
-  function success(position: {
-    coords: { latitude: number; longitude: number };
-  }) {
-    const lat = position.coords.latitude;
-    const lon = position.coords.longitude;
-
-    dispatch({
-      type: weatherSagasActions.FETCH_WEATHER,
-      payload: { lat, lon },
-    });
-  }
-
-  function error() {
-    console.log("La geolocalización no está disponible en este navegador.");
-  }
 
   useEffect(() => {
-    if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition(success, error);
-    } else {
-      error();
+    async function getLocation() {
+      try {
+        const permission = await navigator.permissions.query({
+          name: "geolocation",
+        });
+
+        if (permission.state === "granted") {
+          navigator.geolocation.getCurrentPosition(
+            (position) => {
+              const lat = position.coords.latitude;
+              const lon = position.coords.longitude;
+
+              dispatch({
+                type: weatherSagasActions.FETCH_WEATHER,
+                payload: { lat, lon },
+              });
+            },
+            () => {
+              dispatch({
+                type: weatherActions.setError.type,
+                payload: "No se pudo obtener la ubicación actual.",
+              });
+            }
+          );
+        } else if (permission.state === "prompt") {
+          if (window.confirm("¿Permitir acceso a la ubicación?")) {
+            navigator.geolocation.getCurrentPosition(
+              (position) => {
+                const lat = position.coords.latitude;
+                const lon = position.coords.longitude;
+
+                dispatch({
+                  type: weatherSagasActions.FETCH_WEATHER,
+                  payload: { lat, lon },
+                });
+              },
+              () => {
+                dispatch({
+                  type: weatherActions.setError.type,
+                  payload: "No se pudo obtener la ubicación actual.",
+                });
+              }
+            );
+          } else {
+            dispatch({
+              type: weatherActions.setError.type,
+              payload: "Permiso de geolocalización denegado.",
+            });
+          }
+        } else {
+          dispatch({
+            type: weatherActions.setError.type,
+            payload: "La geolocalización no está disponible en este navegador.",
+          });
+        }
+      } catch (error) {
+        console.error("Error al solicitar permiso de geolocalización:", error);
+        dispatch({
+          type: weatherActions.setError.type,
+          payload: "Ocurrió un error al obtener permiso de geolocalización.",
+        });
+      }
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+
+    getLocation();
+  }, [dispatch]);
 }
